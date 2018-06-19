1aec6ac550e8de143f5f66b654770936a0713510
ChrisEskridge <Chris@ChrisEskridge.com>
2018-06-18 22:12:24 -0400

    Update collections-library.php

--- collections-library.php
+++ collections-library.php
@@ -5,167 +5,58 @@ Template Name: Collections-Library
 
 get_header(); ?>
 
-	<div id="content" class="site-content">
-    	<div class="slider">
-        	<div class="slider-box">
-            	<div class="pic-text">
-                	<span style="color:#dbd2be;">Collections |</span> Library
-                </div>
-            	<img src="<?php bloginfo('template_directory'); ?>/images/about.jpg" style="width:100%;">	
-            </div>
-        </div>
+<div id="content" class="site-content">
+  <div class="slider">
+    <div class="slider-box">
+      <div class="pic-text"> <span style="color:#dbd2be;">Collections |</span> Library </div>
+      <img src="<?php bloginfo('template_directory'); ?>/images/about.jpg" style="width:100%;"> </div>
+  </div>
+  <div class="about-1">
+    <div class="left-color"> </div>
+    <div class="restricter2">
+      <div class="right-third-hidden">
+        <div class="pic-box"> <img src="<?php bloginfo('template_directory'); ?>/images/lr.jpg" style="width:100%;"> </div>
+      </div>
+      <div class="left-2third-table"> The Lauren Rogers Museum of Art in Laurel, Mississippi, was opened in 1923 as a memorial to Lauren Eastman Rogers, the only son and only grandson of one of the town's founding families. Lauren had died in 1921 from complications of appendicitis at the age of 23. After his death, Lauren's father, Wallace Brown Rogers, and his grandfather, Lauren Chase Eastman, created the Eastman Memorial Foundation "to promote the public welfare by founding, endowing and having maintained a public library, museum, art gallery and educational institution, within the state of Mississippi."
         
-        <div class="about-1">
-        	<div class="left-color">
-            </div>
-        	<div class="restricter2">
-                <div class="right-third-hidden">
-                    <div class="pic-box">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/lr.jpg" style="width:100%;">
-                    </div>
-                </div>
-                <div class="left-2third-table">
-                    The Lauren Rogers Museum of Art in Laurel, Mississippi, was opened in 1923 as a memorial to Lauren Eastman Rogers, the only son and only grandson of one of the town's founding families. Lauren had died in 1921 from complications of appendicitis at the age of 23. After his death, Lauren's father, Wallace Brown Rogers, and his grandfather, Lauren Chase Eastman, created the Eastman Memorial Foundation "to promote the public welfare by founding, endowing and having maintained a public library, museum, art gallery and educational institution, within the state of Mississippi."
-    
-    The Eastman, Gardiner and Rogers families had come to Laurel from Clinton, Iowa, in the 1890s in search of uncut timber. Their influence on the town touched all aspects of the residents' lives: economic, social, educational and aesthetic. All expectations were that Lauren Rogers would assume an important role in the community, taking a leadership role in business and contributing to the general well-being of the community as well. Deeply grieved by his untimely death, Lauren's family was determined that something good should come of the tragedy. The end result of their vision and generosity is the Lauren Rogers Museum of Art, which sits on the site where Lauren was building a home for his new bride, Lelia. 
-                </div>
-                <div class="right-third-hidden2">
-                    <div class="pic-box">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/lr.jpg" style="width:100%;">
-                    </div>
-                </div>
-            </div>
-        </div>
+        The Eastman, Gardiner and Rogers families had come to Laurel from Clinton, Iowa, in the 1890s in search of uncut timber. Their influence on the town touched all aspects of the residents' lives: economic, social, educational and aesthetic. All expectations were that Lauren Rogers would assume an important role in the community, taking a leadership role in business and contributing to the general well-being of the community as well. Deeply grieved by his untimely death, Lauren's family was determined that something good should come of the tragedy. The end result of their vision and generosity is the Lauren Rogers Museum of Art, which sits on the site where Lauren was building a home for his new bride, Lelia. </div>
+      <div class="right-third-hidden2">
+        <div class="pic-box"> <img src="<?php bloginfo('template_directory'); ?>/images/lr.jpg" style="width:100%;"> </div>
+      </div>
+    </div>
+  </div>
+  <div class="about-2">
+    <div class="restricter2">
+      <div class="left-third-clear2">
+        <div class="pic-box"> <img src="<?php bloginfo('template_directory'); ?>/images/lr.jpg" style="width:100%;"> </div>
+      </div>
+      <div class="right-2third-clear"> Located on a broad, tree-lined avenue among turn-of-the-century homes near the center of town, the Lauren Rogers Museum of Art is a Georgian Revival structure designed by New Orleans architect Rathbone deBuys. Large, double-hung sash windows accentuate an exterior of local brick with Indiana limestone. The slender, attenuated metal columns in front were made locally by the Laurel Machine and Foundry Company. 
         
-        <div class="about-2">
-        	
-        	<div class="restricter2">
-                <div class="left-third-clear2">
-                    <div class="pic-box">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/lr.jpg" style="width:100%;">
-                    </div>
-                </div>
-                <div class="right-2third-clear">
-                    Located on a broad, tree-lined avenue among turn-of-the-century homes near the center of town, the Lauren Rogers Museum of Art is a Georgian Revival structure designed by New Orleans architect Rathbone deBuys. Large, double-hung sash windows accentuate an exterior of local brick with Indiana limestone. The slender, attenuated metal columns in front were made locally by the Laurel Machine and Foundry Company. 
-    
-    The interior of the building utilized the expertise of the Chicago interior design firm of Watson and Walton. The walls are paneled in quarter-sawn golden oak, accented by handwrought ironwork by Samuel Yellin and a ceiling of handmolded plaster. Cork floors are found throughout the Museum. 
-    
-    The Eastman Memorial Foundation initially sought to establish a public library for Laurel and Jones County in the building and decided to add a museum wing after construction had begun. Today, the Lauren Rogers Museum of Art includes an extensive art and local history library with more than 10,000 volumes, but the primary focus of the Museum is its collections. Contributions from the private collections of the founding families formed the original collection, and these works remain at the core of the Museum's holdings. The first donation was an assortment of 494 Native American baskets, collected by Lauren's great-aunt, Catherine Marshall Gardiner. Currently, the Catherine Marshall Gardiner Native American Basket Collection includes about 800 baskets, including 500 from North America.
-                </div>
-            </div>
-        </div>
+        The interior of the building utilized the expertise of the Chicago interior design firm of Watson and Walton. The walls are paneled in quarter-sawn golden oak, accented by handwrought ironwork by Samuel Yellin and a ceiling of handmolded plaster. Cork floors are found throughout the Museum. 
         
-        <div class="about-1">
-        	<div class="left-color">
-            </div>
-        	<div class="restricter2">
-            
-            	<div class="right-third-hidden">
-                    <div class="pic-box">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/lr.jpg" style="width:100%;">
-                    </div>
-                </div>
-                <div class="left-2third-table">
-                    The Rogers and Eastman families donated important 19th and 20th century paintings by such noteworthy American artists as Winslow Homer, Albert Bierstadt, George Inness, John Frederick Kensett and Ralph Albert Blakelock. Works by John H. Twachtman and John Singer Sargent were acquired soon thereafter. Today, the Museum displays one of the finest collections of 19th and 20th century European and American works to be found in the South. The Museum's European Gallery features paintings by Jean Francois Millet, Jean-Baptiste Camille Corot and Jules Adolph Breton, among others, and the American Gallery includes works by Thomas Moran, Mary Cassatt, Charles Hawthorne, John Sloan and others. The Museum's contemporary collection includes works by Romare Bearden, Marie Hull, Walter Anderson, Ida Kohlmeyer and other artists of national and regional renown.
-    
-    Another gift of the Museum's founders was the donation of 142 Japanese Ukiyo-e woodblock prints from the 18th and 19th centuries. Given by Lauren's father, Wallace B. Rogers, this outstanding collection features work by such masters as Harunobu, Masanobu, Utamaro, Eishi, Hokusai and Hiroshige and attracts visitors from throughout the United States.
-    
-    In the 1970s, Thomas and Harriet Gibbons, co-owners of the local newspaper, donated their extensive collection of sterling silver. The Gibbons English Georgian Silver Collection includes 65 major objects associated with the serving of tea, including tea caddies, tea and coffee pots, baskets for cakes and sweetmeats, and salvers. Works by John Gibbons, William Plummer, Hester Bateman, John Scofield and Henry Greenway are included. 
-                </div>
-                <div class="right-third-hidden2">
-                    <div class="pic-box">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/lr.jpg" style="width:100%;">
-                    </div>
-                </div>
-            
-            
-            
-                <!--<div class="right-third">
-                    <div class="pic-box">
-                        <img src="<?php bloginfo('template_directory'); ?>/images/lr.jpg" style="width:100%;">
-                    </div>
-                    
-                </div>
-                <div class="left-2third">
-                    The Rogers and Eastman families donated important 19th and 20th century paintings by such noteworthy American artists as Winslow Homer, Albert Bierstadt, George Inness, John Frederick Kensett and Ralph Albert Blakelock. Works by John H. Twachtman and John Singer Sargent were acquired soon thereafter. Today, the Museum displays one of the finest collections of 19th and 20th century European and American works to be found in the South. The Museum's European Gallery features paintings by Jean Francois Millet, Jean-Baptiste Camille Corot and Jules Adolph Breton, among others, and the American Gallery includes works by Thomas Moran, Mary Cassatt, Charles Hawthorne, John Sloan and others. The Museum's contemporary collection includes works by Romare Bearden, Marie Hull, Walter Anderson, Ida Kohlmeyer and other artists of national and regional renown.
-    
-    Another gift of the Museum's founders was the donation of 142 Japanese Ukiyo-e woodblock prints from the 18th and 19th centuries. Given by Lauren's father, Wallace B. Rogers, this outstanding collection features work by such masters as Harunobu, Masanobu, Utamaro, Eishi, Hokusai and Hiroshige and attracts visitors from throughout the United States.
-    
-    In the 1970s, Thomas and Harriet Gibbons, co-owners of the local newspaper, donated their extensive collection of sterling silver. The Gibbons English Georgian Silver Collection includes 65 major objects associated with the serving of tea, including tea caddies, tea and coffee pots, baskets for cakes and sweetmeats, and salvers. Works by John Gibbons, William Plummer, Hester Bateman, John Scofield and Henry Greenway are included.  
-                </div>-->
-            </div>
-            
-        </div>
+        The Eastman Memorial Foundation initially sought to establish a public library for Laurel and Jones County in the building and decided to add a museum wing after construction had begun. Today, the Lauren Rogers Museum of Art includes an extensive art and local history library with more than 10,000 volumes, but the primary focus of the Museum is its collections. Contributions from the private collections of the founding families formed the original collection, and these works remain at the core of the Museum's holdings. The first donation was an assortment of 494 Native American baskets, collected by Lauren's great-aunt, Catherine Marshall Gardiner. Currently, the Catherine Marshall Gardiner Native American Basket Collection includes about 800 baskets, including 500 from North America. </div>
+    </div>
+  </div>
+  <div class="about-1">
+    <div class="left-color"> </div>
+    <div class="restricter2">
+      <div class="right-third-hidden">
+        <div class="pic-box"> <img src="<?php bloginfo('template_directory'); ?>/images/lr.jpg" style="width:100%;"> </div>
+      </div>
+      <div class="left-2third-table"> The Rogers and Eastman families donated important 19th and 20th century paintings by such noteworthy American artists as Winslow Homer, Albert Bierstadt, George Inness, John Frederick Kensett and Ralph Albert Blakelock. Works by John H. Twachtman and John Singer Sargent were acquired soon thereafter. Today, the Museum displays one of the finest collections of 19th and 20th century European and American works to be found in the South. The Museum's European Gallery features paintings by Jean Francois Millet, Jean-Baptiste Camille Corot and Jules Adolph Breton, among others, and the American Gallery includes works by Thomas Moran, Mary Cassatt, Charles Hawthorne, John Sloan and others. The Museum's contemporary collection includes works by Romare Bearden, Marie Hull, Walter Anderson, Ida Kohlmeyer and other artists of national and regional renown.
         
-        <div class="european-pic">
-        	  
-        </div>
+        Another gift of the Museum's founders was the donation of 142 Japanese Ukiyo-e woodblock prints from the 18th and 19th centuries. Given by Lauren's father, Wallace B. Rogers, this outstanding collection features work by such masters as Harunobu, Masanobu, Utamaro, Eishi, Hokusai and Hiroshige and attracts visitors from throughout the United States.
         
-        
-        
-        
-            
-            
-            
-        	<!--<div class="exhibition">
-                <div class="restricter2">
-                    <div class="ec-title">
-                        <div class="sub-top-shadow">
-                        </div>
-                        
-                        <div class="title-text">
-                            Exhibitions
-                        </div>
-                        <div class="more-box">
-                            MORE >>
-                        </div>
-                        <div class="title-bar">
-                            
-                        </div>
-                        </div>
-                    
-                    
-                    <div class="ec-content">
-                    </div>
-                    
-                </div>
-            </div>
-            <div class="collection">
-                
-                    <div class="ec-title">
-                        <div class="sub-top-shadow">
-                        </div>
-                        <div class="restricter3">
-                        <div class="title-text">
-                            Collections
-                        </div>
-                        <div class="more-box">
-                            MORE >>
-                        </div>
-                        <div class="title-bar">
-                            
-                        </div>
-                    
-                    <div class="ec-content">
-                    </div>
-                    </div>
-                </div>
-            </div>-->
-        <!--<div class="subscribe">
-        	<div class="sub-top-shadow">
-            </div>
-            <div class="sub-wrap">
-                <div class="sub-text">
-                    Subscribe to the LRMA e-newsletter
-                </div>
-                <div class="sub-button">
-                        SIGN UP
-                </div>
-            </div>
-            <div class="sub-bottom-shadow">
-            </div>
-        </div>-->
-    </div><!-- #content -->
+        In the 1970s, Thomas and Harriet Gibbons, co-owners of the local newspaper, donated their extensive collection of sterling silver. The Gibbons English Georgian Silver Collection includes 65 major objects associated with the serving of tea, including tea caddies, tea and coffee pots, baskets for cakes and sweetmeats, and salvers. Works by John Gibbons, William Plummer, Hester Bateman, John Scofield and Henry Greenway are included. </div>
+      <div class="right-third-hidden2">
+        <div class="pic-box"> <img src="<?php bloginfo('template_directory'); ?>/images/lr.jpg" style="width:100%;"> </div>
+      </div>
+    </div>
+  </div>
+  <div class="european-pic"> </div>
+</div>
+<!-- #content -->
 
 <?php
 get_footer();
+
