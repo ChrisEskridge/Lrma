0f2ce5d07e59f6e9b7a195ddd343cc831c481281
ChrisEskridge <Chris@ChrisEskridge.com>
2018-06-18 21:49:34 -0400

    Update archive.php

--- archive.php
+++ archive.php
@@ -9,20 +9,19 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
+    <?php
 		if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
+    <header class="page-header">
+      <?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
-			</header><!-- .page-header -->
-
-			<?php
+    </header>
+    <!-- .page-header -->
+    
+    <?php
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
 
@@ -42,10 +41,12 @@ get_header(); ?>
 			get_template_part( 'template-parts/content', 'none' );
 
 		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+  </main>
+  <!-- #main --> 
+</div>
+<!-- #primary -->
 
 <?php
 get_sidebar();
 get_footer();
+
