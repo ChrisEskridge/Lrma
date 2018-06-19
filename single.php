e8813ce2d7bce7d8510be483b99f2475e38eae72
ChrisEskridge <Chris@ChrisEskridge.com>
2018-06-19 10:24:49 -0400

    Update single.php

--- single.php
+++ single.php
@@ -9,10 +9,9 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
+    <?php
 		while ( have_posts() ) : the_post();
 
 			get_template_part( 'template-parts/content', get_post_format() );
@@ -26,10 +25,12 @@ get_header(); ?>
 
 		endwhile; // End of the loop.
 		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+  </main>
+  <!-- #main --> 
+</div>
+<!-- #primary -->
 
 <?php
 get_sidebar();
 get_footer();
+
