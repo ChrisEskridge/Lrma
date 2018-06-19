e95c3734d5f2afcb38799a6bd2954b47611dc290
ChrisEskridge <Chris@ChrisEskridge.com>
2018-06-19 10:15:42 -0400

    Update index.php

--- index.php
+++ index.php
@@ -14,18 +14,18 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
+    <?php
 		if ( have_posts() ) :
 
 			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-
-			<?php
+    <header>
+      <h1 class="page-title screen-reader-text">
+        <?php single_post_title(); ?>
+      </h1>
+    </header>
+    <?php
 			endif;
 
 			/* Start the Loop */
@@ -47,10 +47,12 @@ get_header(); ?>
 			get_template_part( 'template-parts/content', 'none' );
 
 		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+  </main>
+  <!-- #main --> 
+</div>
+<!-- #primary -->
 
 <?php
 get_sidebar();
 get_footer();
+
